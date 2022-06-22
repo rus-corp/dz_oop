9eeeb41bc2437b7fe34927b26091f2b2f504dca8
Руслан Прусаков <ruslan.prusakov@gmail.com>
2022-06-22 23:54:52 +0300

    nothing do

--- dz_oop.py
+++ dz_oop.py
@@ -21,6 +21,16 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+
+class Lecturer(Mentor):
+  pass
+
+
+class Reviewer(Mentor):
+  pass
+
+
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
@@ -33,6 +43,3 @@ cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
  
 print(best_student.grades)
-
-
-
