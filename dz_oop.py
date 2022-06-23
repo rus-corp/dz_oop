28eec98b8a24b71aa9661303a9beef55b7d17471
Руслан Прусаков <ruslan.prusakov@gmail.com>
2022-06-23 12:27:04 +0300

    punkt3

--- dz_oop.py
+++ dz_oop.py
@@ -6,13 +6,53 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    # def rate_av(self, student, course, grade):
+
+    # def __str__(self):
+    #     res = f'Имя: {self.name}\n Фамилия:{self.surname}\n Средняя оценка за домашние задания: {}\n Курсы в процессе изучения: {self.courses_in_progress}\n Завершенные курсы: {self.finished_courses}'
+    #     return res
+
+
         
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
-        self.courses_attached = []
         
+        
+        
+
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.courses_grades = {}
+    
+    # def __str__(self):
+    #     res = f' Имя: {self.name}\n Фамилия {self.surname}\n Средняя оценка за лекции {}'
+    #     return res
+
+
+  
+
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.courses_attached = []
+
+
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -22,24 +62,31 @@ class Mentor:
         else:
             return 'Ошибка'
 
-
-class Lecturer(Mentor):
-  pass
-
-
-class Reviewer(Mentor):
-  pass
+    def __str__(self):
+        res = f' Имя: {self.name}\n Фамилия: {self.surname}'
+        return res
+  
 
 
  
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.courses_in_progress += ['Python']
  
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+# cool_mentor = Mentor('Some', 'Buddy')
+# cool_mentor.courses_attached += ['Python']
  
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
  
-print(best_student.grades)
+# print(best_student.grades)
+
+rus = Student('Ruslan', 'Prusakov', 'mens')
+rus.courses_in_progress += ['Python']
+
+
+
+print(rus.name)
+print(rus.surname)
+print(rus.courses_in_progress)
+
