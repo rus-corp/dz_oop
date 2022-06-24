e179f8a5d844efbc2edecbc42c9a0c6c0b926f7e
Руслан Прусаков <ruslan.prusakov@gmail.com>
2022-06-24 19:58:37 +0300

    add lt

--- dz_oop.py
+++ dz_oop.py
@@ -12,23 +12,32 @@ class Student:
 
 
     def av_grade(self):
-        new_list = []
+        stud_list = []
         for grade in self.grades.values():
-            new_list.extend(grade)
-        res = round(sum(new_list) / len(new_list), 1)
-        return res
+            stud_list.extend(grade)
+        res_3 = round(sum(stud_list) / len(stud_list), 1)
+        return res_3
 
     
 
 
     def rate_hw(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in self.finished_courses:
-            if course in lecturer.grades:
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.ending_courses:
+            if course in lecturer.courses_grades:
                 lecturer.courses_grades[course] += [grade]
             else:
                 lecturer.courses_grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Not a Student')
+            return
+        return self.av_grade() < other.av_grade()
+
+    
+
 
 
 
@@ -53,15 +62,21 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.courses_grades = {}
-        self.finished_courses = []
+        self.ending_courses = []
 
 
     def lector_grades(self):
-        new_list = []
+        lec_list = []
         for grade in self.courses_grades.values():
-            new_list.extend(grade)
-        res = round(sum(new_list) / len(new_list), 1)
-        return res
+            lec_list.extend(grade)
+        res_2 = round(sum(lec_list) / len(lec_list), 1)
+        return res_2
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Not a Lector')
+            return
+        return self.lector_grades() < other.lector_grades()
 
     
     def __str__(self):
@@ -70,7 +85,6 @@ class Lecturer(Mentor):
 
 
   
-
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -110,9 +124,15 @@ rus = Student('Ruslan', 'Prusakov', 'mens')
 rus.courses_in_progress += ['Python']
 rus.courses_in_progress += ['Git']
 
+ven = Student('Venera', 'Karimova', 'girl')
+ven.courses_in_progress += ['Python']
+ven.courses_in_progress += ['Git']
+
 
 
 first_reviewer = Reviewer('Ven', 'Kar')
+first_reviewer.courses_attached += ['Python']
+first_reviewer.courses_attached += ['Git']
 first_reviewer.rate_hw(rus, 'Python', 10)
 first_reviewer.rate_hw(rus, 'Python', 9)
 first_reviewer.rate_hw(rus, 'Python', 7)
@@ -120,9 +140,17 @@ first_reviewer.rate_hw(rus, 'Git', 4)
 first_reviewer.rate_hw(rus, 'Git', 6)
 first_reviewer.rate_hw(rus, 'Git', 8)
 
+first_reviewer.rate_hw(ven, 'Python', 6)
+first_reviewer.rate_hw(ven, 'Python', 5)
+first_reviewer.rate_hw(ven, 'Python', 8)
+first_reviewer.rate_hw(ven, 'Git', 3)
+first_reviewer.rate_hw(ven, 'Git', 2)
+first_reviewer.rate_hw(ven, 'Git', 9)
+
+
 first_lector = Lecturer('Kar', 'Bik')
-first_lector.finished_courses += ['Python']
-first_lector.finished_courses += ['Git']
+first_lector.ending_courses += ['Python']
+first_lector.ending_courses += ['Git']
 rus.rate_hw(first_lector, 'Python', 6)
 rus.rate_hw(first_lector, 'Python', 8)
 rus.rate_hw(first_lector, 'Python', 2)
@@ -130,15 +158,15 @@ rus.rate_hw(first_lector, 'Git', 10)
 rus.rate_hw(first_lector, 'Git', 9)
 rus.rate_hw(first_lector, 'Git', 7)
 
-print(first_lector.courses_grades)
-print(first_lector.finished_courses)
-
-
-
-
-# print(rus)
+print(rus)
+print()
+print(ven)
+print(rus > ven)
+# print()
 # print(first_reviewer)
+# print()
 # print(first_lector)
+# print()
 
 
 # brus = Reviewer ('Papa', 'Djons')
